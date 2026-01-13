0040be4ac1d2a5a4dc2ab46170963e509db77d46
EC2 Default User <ec2-user@ip-172-31-25-228.us-west-1.compute.internal>
2026-01-13 23:18:38 +0000

    Initial commit: app, tests, and CI pipeline

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from app import add, subtract
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(0, 0) == 0
+    assert add(-1, 1) == 0
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(0, 0) == 0
+    assert subtract(-1, 1) == -2
